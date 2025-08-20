55dd21d49af1c3277ad2f71393ae15d03dc96e23
tristenkelly <tristenkelly98@gmail.com>
2025-08-19 20:55:34 -0400

    gitignore activities

--- dist/command_exit.js
+++
@@ -1,6 +0,0 @@
-export async function commandExit(state) {
-    console.log("Closing the Pokedex... Goodbye!");
-    state.readline.close();
-    process.exit(0);
-}
-;
--- dist/main.js
+++
@@ -1,7 +0,0 @@
-import { startREPL } from "./repl.js";
-import { initState } from "./state.js";
-function main() {
-    const state = initState();
-    startREPL(state);
-}
-main();
